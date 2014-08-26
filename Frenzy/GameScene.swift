899acd1a07fe40373d9092e028bf3eaf992e3338
Samuel Wejeus <samuel.wejeus@bontouch.com>
2014-08-26 22:38:36 +0200

    Initial

---
+++ Frenzy/GameScene.swift
@@ -0,0 +1,124 @@
+//
+//  GameScene.swift
+//  Frenzy
+//
+//  Created by Samuel Wejéus on 26/08/14.
+//  Copyright (c) 2014 Rocket Labs. All rights reserved.
+//
+
+import CoreMotion
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    let MAX_NUM_CIRCLES = 3
+    let MAX_CIRCLE_SCALE:CGFloat = 10
+    let margin:CGFloat = 100
+    
+    let motionManager = CMMotionManager()
+    let colors = [SKColor.blackColor(), SKColor.brownColor(), SKColor.blueColor()]
+    
+    var numCircles = 0
+    
+    var isPlaying:Bool = false
+    var score = 0
+    var playerLife = 10;
+    
+    var level:Int = 1
+    var levelSpeed:CGFloat = 0.02
+    var scoreLevelMultiplier = 1.5
+    var numKilled:Int = 0
+    let levelCap:Int = 10
+    
+    /* Setup your scene here */
+    override func didMoveToView(view: SKView) {
+        self.physicsBody = SKPhysicsBody(edgeLoopFromRect: self.frame)
+        self.backgroundColor = SKColor.whiteColor()
+        isPlaying = true
+        motionManager.startDeviceMotionUpdates()
+    }
+    
+    /* Called when a touch begins */
+    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
+        for touch: AnyObject in touches {
+            
+            let location = touch.locationInNode(self)
+            
+            var touchedNode = self.nodeAtPoint(location)
+            if touchedNode.isKindOfClass(SKShapeNode) {
+                let actionScale = SKAction.scaleBy(
+                    CGFloat(7.0),
+                    duration: 0.2
+                )
+                let actionDissolve = SKAction.fadeAlphaTo(0, duration:0.2)
+                
+                let actionRemove = SKAction.runBlock({
+                    touchedNode.removeFromParent()
+                    self.numCircles--;
+                    self.numKilled++
+                    
+                    println("numKilled: \(self.numKilled)")
+                })
+                
+                let chain = SKAction.sequence([actionScale, actionRemove])
+                
+                touchedNode.runAction(actionDissolve)
+                touchedNode.runAction(chain)
+            }
+        }
+    }
+    
+    // Can also search for sprite in tree! var sprite = self.childNodeWithName("MyJetSprite"); // uses sprite.name = "x" to find (can also use patterns)
+    func levelUp() {
+        level++
+        levelSpeed += levelSpeed
+        scoreLevelMultiplier += scoreLevelMultiplier
+        numKilled = 0
+    }
+    
+    /* Called before each frame is rendered */
+    override func update(currentTime: CFTimeInterval) {
+        if numKilled == levelCap {
+            levelUp()
+        }
+        
+        if isPlaying {
+            // create new nodes
+            if (numCircles < MAX_NUM_CIRCLES) {
+                let circle = SKShapeNode(circleOfRadius: 15)
+                
+                var colorId = Int(arc4random_uniform(2))
+                circle.fillColor = colors[colorId]
+                
+                let randX = CGFloat(arc4random_uniform(UInt32(self.frame.width - margin)))
+                let randY = CGFloat(arc4random_uniform(UInt32(self.frame.height - margin)))
+                
+                circle.position = CGPoint(x: randX + margin/2 , y: randY + margin/2)
+                self.addChild(circle)
+                numCircles++
+            }
+            
+            // increase size of existing nodes
+            for node in self.children {
+                if let shapeNode = node as? SKShapeNode {
+                    shapeNode.xScale += levelSpeed
+                    shapeNode.yScale += levelSpeed
+                    
+                    // test if circle have exploded
+                    
+                    // TODO! So this clearly does not work since when we are removing the node
+                    // we do animation that scale the fuck of of this fucker and this will break
+                    if shapeNode.xScale > MAX_CIRCLE_SCALE {
+                        // playerLife--
+                        
+                        if playerLife == 0 {
+                            println("game over!")
+                            isPlaying = false
+                            break
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Frenzy/GameViewController.swift
@@ -0,0 +1,57 @@
+//
+//  GameViewController.swift
+//  Frenzy
+//
+//  Created by Samuel Wejéus on 26/08/14.
+//  Copyright (c) 2014 Rocket Labs. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        /* Pick a size for the scene */
+        let scene = GameScene();
+        
+        // Configure the view.
+        let skView = self.view as SKView
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        
+        /* Sprite Kit applies additional optimizations to improve rendering performance */
+        skView.ignoresSiblingOrder = true
+        
+        /* Set the scale mode to scale to fit the window */
+        scene.scaleMode = .AspectFill
+        
+        scene.size = skView.bounds.size
+        
+        
+        skView.presentScene(scene)
+    }
+    
+    override func shouldAutorotate() -> Bool {
+        return true
+    }
+    
+    override func supportedInterfaceOrientations() -> Int {
+        if UIDevice.currentDevice().userInterfaceIdiom == .Phone {
+            return Int(UIInterfaceOrientationMask.AllButUpsideDown.toRaw())
+        } else {
+            return Int(UIInterfaceOrientationMask.All.toRaw())
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Release any cached data, images, etc that aren't in use.
+    }
+    
+    override func prefersStatusBarHidden() -> Bool {
+        return true
+    }
+}

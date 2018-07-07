b59953a43b2d0a3a1839138b7e1c1fa65eadda5e
Samuel Wejéus <sawe@tracer.lan>
2018-07-07 11:38:47 +0200

    Gameplay changes

--- Frenzy/GameScene.swift
+++ Frenzy/GameScene.swift
@@ -25,18 +25,24 @@ class GameScene: SKScene {
     
     let motionManager = CMMotionManager()
     
-    var numCircles = 0
+    var playerOneNumCircles = 0
+    var playerTwoNumCircles = 0
     
     var isPlaying:Bool = false
     
     
-    var score:Int = 0
-    var playerLife = 10;
-    var numKilled:Int = 0
-    let levelCap:Int = 10
+    var playerOneScore:Int = 0
+    var playerOneLife = 10;
+    
+
+    var playerTwoScore:Int = 0
+    var playerTwoLife = 10
+
+    
     var lives:UILabel = UILabel()
     var scoreText:UILabel = UILabel()
-    
+    var numKilled:Int = 0
+    let levelCap:Int = 1
     
     var level:Int = 1
     var levelSpeed:CGFloat = 0.02
@@ -69,43 +75,41 @@ class GameScene: SKScene {
         }
         
         lives = UILabel(frame: CGRect(x: 5, y: 0, width: 200, height: 21))
-        lives.text = "Lives: " + String(playerLife)
+        lives.text = "Lives - P1: \(String(playerOneLife)) P2: \(String(playerTwoLife))"
         lives.textColor = UIColor.white
         self.view?.addSubview(lives)
-        
+
         scoreText = UILabel(frame: CGRect(x: self.frame.width-100, y: 0, width: 400, height: 21)) // eh
-        scoreText.text = "Score: "+String(score)
+        lives.text = "Score - P1: \(String(playerOneScore)) P2: \(String(playerTwoScore))"
         scoreText.textColor = .white
         self.view?.addSubview(scoreText)
-        isPlaying = true
         
-
-    
+        isPlaying = true
         motionManager.startDeviceMotionUpdates()
     }
     
     func showFaster() {
-        let fasterTextLabel = SKLabelNode()
-        
-        fasterTextLabel.text = "Faster!!"
-        fasterTextLabel.fontName = "Something Strange"
-        fasterTextLabel.fontSize = 40
-        fasterTextLabel.position = CGPoint(x: (self.frame.width/2)+50, y: (self.frame.height/2)-100)
-        fasterTextLabel.color = SKColor.white
-
-        self.addChild(fasterTextLabel)
-
-
-        let action = SKAction.scale(to: 80, duration: 6)
-
-        let action2 = SKAction.fadeAlpha(to: 0, duration: 1.5)
-        
-        let removeAction = SKAction.run({
-            fasterTextLabel.removeFromParent()
-        })
-        
-        fasterTextLabel.run(action2)
-        fasterTextLabel.run(SKAction.sequence([action, removeAction]))
+//        let fasterTextLabel = SKLabelNode()
+//
+//        fasterTextLabel.text = "Faster!!"
+//        fasterTextLabel.fontName = "Something Strange"
+//        fasterTextLabel.fontSize = 40
+//        fasterTextLabel.position = CGPoint(x: (self.frame.width/2)+50, y: (self.frame.height/2)-100)
+//        fasterTextLabel.color = SKColor.white
+//
+//        self.addChild(fasterTextLabel)
+//
+//
+//        let action = SKAction.scale(to: 80, duration: 6)
+//
+//        let action2 = SKAction.fadeAlpha(to: 0, duration: 1.5)
+//
+//        let removeAction = SKAction.run({
+//            fasterTextLabel.removeFromParent()
+//        })
+//
+//        fasterTextLabel.run(action2)
+//        fasterTextLabel.run(SKAction.sequence([action, removeAction]))
     }
 
     func showGameOver() {
@@ -120,10 +124,14 @@ class GameScene: SKScene {
     }
     
     func reset() {
-        playerLife = 10
-        score = 0
-        lives.text = "Lives: "+String(playerLife)
-        scoreText.text = "Score: "+String(score)
+        playerOneLife = 10
+        playerOneScore = 0
+        playerTwoLife = 10
+        playerTwoScore = 0
+    
+        
+        lives.text = "Lives - P1: \(String(playerOneLife)) P2: \(String(playerTwoLife))"
+        lives.text = "Score - P1: \(String(playerOneScore)) P2: \(String(playerTwoScore))"
         
         level = 1
         levelSpeed = 0.02
@@ -140,11 +148,17 @@ class GameScene: SKScene {
                 shapeNode.removeFromParent();
             }
         }
-        numCircles = 0
+        
+        playerOneNumCircles = 0
+        playerTwoNumCircles = 0
     }
     
     /* Called when a touch begins */
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        if !isPlaying {
+            return
+        }
+        
         for touch: UITouch in touches {
             
             let location = touch.location(in: self)
@@ -163,13 +177,20 @@ class GameScene: SKScene {
                     
                     let actionRemove = SKAction.run({
                         touchedNode.removeFromParent()
-                        self.numCircles -= 1
                         self.numKilled += 1
-                        self.score += 1
+                        
+                        if location.x < self.frame.width/2 {
+                            self.playerOneScore += 1
+                            self.playerOneNumCircles -= 1
+                        } else {
+                            self.playerTwoScore += 1
+                            self.playerTwoNumCircles -= 1
+                        }
+                        
                         // this multiplier is kind of aggressive
                         // var actualScore:Double = Double(Double(self.score) * self.scoreLevelMultiplier)
                         // self.score = Int(actualScore)
-                        self.scoreText.text = "Score: "+String(self.score)
+                        self.lives.text = "Score - P1: \(String(self.playerOneScore)) P2: \(String(self.playerTwoScore))"
                         
                         print("numKilled: \(self.numKilled)")
                     })
@@ -204,26 +225,54 @@ class GameScene: SKScene {
             levelUp()
         }
         
+        if !isPlaying {
+            return
+        }
+        
         if isPlaying {
             // create new nodes
-            if (numCircles < MAX_NUM_CIRCLES) {
-                let circle = SKShapeNode(circleOfRadius: 15)
-                circle.name = "circle"
+            if (playerOneNumCircles < MAX_NUM_CIRCLES && playerTwoNumCircles < MAX_NUM_CIRCLES) {
                 
-                circle.fillColor = SKColor(hue: CGFloat(arc4random_uniform(255)) / 255.0, saturation: 0.9, brightness: 0.8, alpha: 1.0)
-                circle.strokeColor = SKColor(hue: CGFloat(arc4random_uniform(255)) / 255.0, saturation: 0.9, brightness: 0.8, alpha: 1.0)
                 
-                circle.glowWidth = 1.0
-                circle.lineWidth = 1.0
-                circle.isAntialiased = true
-                circle.alpha = 1
+                if playerOneNumCircles < MAX_NUM_CIRCLES/2 {
+                    let circle = SKShapeNode(circleOfRadius: 15)
+                    circle.name = "circle"
+                    
+                    circle.strokeColor = SKColor(hue: CGFloat(arc4random_uniform(255)) / 255.0, saturation: 0.9, brightness: 0.8, alpha: 1.0)
+                    
+                    circle.glowWidth = 1.0
+                    circle.lineWidth = 1.0
+                    circle.isAntialiased = true
+                    circle.alpha = 1
+                    
+                    circle.fillColor = SKColor.red
+                    
+                    let randX = CGFloat(arc4random_uniform(UInt32(self.frame.width/2 - margin)))
+                    let randY = CGFloat(arc4random_uniform(UInt32(self.frame.height - margin)))
+                    circle.position = CGPoint(x: randX + margin/2 , y: randY + margin/2)
+                    self.addChild(circle)
+                    playerOneNumCircles += 1
+                }
                 
-                let randX = CGFloat(arc4random_uniform(UInt32(self.frame.width - margin)))
-                let randY = CGFloat(arc4random_uniform(UInt32(self.frame.height - margin)))
-                
-                circle.position = CGPoint(x: randX + margin/2 , y: randY + margin/2)
-                self.addChild(circle)
-                numCircles += 1
+                if playerTwoNumCircles < MAX_NUM_CIRCLES/2 {
+                    let circle = SKShapeNode(circleOfRadius: 15)
+                    circle.name = "circle"
+                    
+                    circle.strokeColor = SKColor(hue: CGFloat(arc4random_uniform(255)) / 255.0, saturation: 0.9, brightness: 0.8, alpha: 1.0)
+                    
+                    circle.glowWidth = 1.0
+                    circle.lineWidth = 1.0
+                    circle.isAntialiased = true
+                    circle.alpha = 1
+                    
+                    circle.fillColor = SKColor.blue
+                    
+                    let randX = CGFloat(arc4random_uniform(UInt32(self.frame.width/2 - margin))) + self.frame.width/2
+                    let randY = CGFloat(arc4random_uniform(UInt32(self.frame.height - margin)))
+                    circle.position = CGPoint(x: randX + margin/2 , y: randY + margin/2)
+                    self.addChild(circle)
+                    playerTwoNumCircles += 1
+                }
             }
             
             // increase size of existing nodes
@@ -239,13 +288,26 @@ class GameScene: SKScene {
                         // test if circle have exploded
                     
                         if shapeNode.xScale > MAX_CIRCLE_SCALE {
-                            playerLife -= 1
-                            lives.text = "Lives: "+String(playerLife)
+                            
+                            if shapeNode.fillColor == SKColor.red {
+                                playerOneLife -= 1
+                                self.playerOneNumCircles -= 1
+                            }
+                            
+                            if shapeNode.fillColor == SKColor.blue {
+                                playerTwoLife -= 1
+                                self.playerTwoNumCircles -= 1
+                            }
+                            
+                            
+                            
+                            lives.text = "Lives - P1: \(String(playerOneLife)) P2: \(String(playerTwoLife))"
                             wompwomp.play()
-                            shapeNode.fillColor = UIColor.red
+                            
+                            shapeNode.fillColor = UIColor.green
                             shapeNode.removeFromParent()
-                            self.numCircles -= 1
-                            if playerLife == 0 {
+                            
+                            if playerOneLife == 0 || playerTwoLife == 0 {
                                 unzunz.stop() // :o
                                 unzunz.currentTime = 0
                                 crybaby.play() // :(
